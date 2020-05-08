05a8b12d9c36737a83d0bd7502b5d8e7a31bbb8c
manojtanwar99 <manojtanwar99@gmail.com>
2020-05-08 20:28:34 +0530

    build

---
+++ build/precache-manifest.43cdeb036eaf4c860eabe57cdfe1448f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e4688e744860c3bd2e85be4b108038a",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "c3b492ccf2eb022b0fc7",
+    "url": "/react/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "3367d618529d45ec9d85",
+    "url": "/react/static/css/main.c52b382e.chunk.css"
+  },
+  {
+    "revision": "c3b492ccf2eb022b0fc7",
+    "url": "/react/static/js/2.33e73699.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/react/static/js/2.33e73699.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3367d618529d45ec9d85",
+    "url": "/react/static/js/main.17b27249.chunk.js"
+  },
+  {
+    "revision": "88e04cedbc67d2aa3912",
+    "url": "/react/static/js/runtime-main.3239f0fb.js"
+  }
+]);
