d74d00ee4c0b965db7bad1235ed644887c3639d5
manojtanwar99 <manojtanwar99@gmail.com>
2020-05-09 14:05:31 +0530

    Updates

--- precache-manifest.ad87d843339096f4f61d7db424f84709.js
+++ precache-manifest.ad87d843339096f4f61d7db424f84709.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f1cfe005b8fa2461b6a48a78bf78f148",
+    "revision": "4f9dd9aecc826796c7eca8bbad6217ad",
     "url": "/reactpro/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/reactpro/static/css/2.69121389.chunk.css"
   },
   {
-    "revision": "0d901135ac1003b9f0d7",
-    "url": "/reactpro/static/css/main.5e23416d.chunk.css"
+    "revision": "5fa7ba7f85e9d49e8b1c",
+    "url": "/reactpro/static/css/main.9c1af1ab.chunk.css"
   },
   {
     "revision": "038a6e6924aa448f4380",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/reactpro/static/js/2.a23e49bf.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0d901135ac1003b9f0d7",
-    "url": "/reactpro/static/js/main.7d0e3a42.chunk.js"
+    "revision": "5fa7ba7f85e9d49e8b1c",
+    "url": "/reactpro/static/js/main.aae564be.chunk.js"
   },
   {
     "revision": "a49eb1dbd1ee11105487",
